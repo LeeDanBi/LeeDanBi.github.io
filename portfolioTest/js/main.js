63dbaaa5b165d7060133983f0c99cbf149ec0f5c
leedanbi <goldq0531@gmail.com>
2017-08-02 00:01:03 +0900

    new페이지 css까지

---
+++ portfolioTest/js/main.js
@@ -0,0 +1,17 @@
+
+
+var goRight = document.querySelector('#goRight');
+var goLeft = document.querySelector('#goLeft');
+var eContain = document.querySelector('#eventBox ul');
+
+goRight.addEventListener('click',EContain);
+function EContain(){
+  eContain.style.transform += "translate(320px,0)";
+  eContain.style.transition = "all 700ms";
+}
+
+goLeft.addEventListener('click',EContain2);
+function EContain2(){
+  eContain.style.transform += "translate(-320px,0)";
+  eContain.style.transition = "all 700ms";
+}
