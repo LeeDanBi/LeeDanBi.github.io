4230b73ba3d3fe3ed9932ba1949799e7dbbd3c57
leedanbi <goldq0531@gmail.com>
2017-08-17 11:55:33 +0900

    20170817

--- portfolioTest/js/sub_men.js
+++ portfolioTest/js/sub_men.js
@@ -2,6 +2,18 @@
 
   
   var tabMenu = $('.tab_menu').children('li');
+  var plBanner = $('.planning_banner');
+
+  var MenuTimerID = setInterval(MenuTimer, 5000);
+
+   var retime=-2540;
+
+  function MenuTimer(){
+  
+    retime -= 1020;
+    plBanner.stop().animate({'marginLeft': retime +'px'},500);
+
+  } 
 
   tabMenu.on('click',function(e){
     e.preventDefault();
@@ -67,3 +79,46 @@
 
   });
 })(this.jQuery);
+
+          function UpperCasy(keyword){
+
+          // var keyword = keyword;
+          var keyword1 = document.querySelector(keyword).innerText;
+          var upperkey = keyword1.toUpperCase();
+
+          var text = upperkey;
+          return keyword1.replace(keyword1,text);
+          }
+
+          document.querySelector('h3').innerText.toUpperCase();
+          // UpperCasy('h3'); //대문자로 바꾸기
+
+          (function($){
+
+            // var text = $('h3').text().replacy().toUpperCase();
+            // $('h3').text(text);
+
+            var goodML = $('#goodsMenu>ul>li');
+            var goodMLL = $('#goodsMenu>ul>li').length;
+
+            for(var i=0;i<goodMLL;i++){
+            var goodU = $('#goodsMenu>ul>li').eq(i)
+                          .find('.menu_content');
+            var goodL = goodU.find('li');
+            var goodL0 = goodL.eq(0);
+            var goodLIn = goodL.length; //각 li마다그 안에 #goodsMenu안의 li갯수를 파악
+            var arr = [];
+             // console.log(goodLIn);
+
+             if(goodLIn <= 2){
+                goodL0.css({marginLeft:'240px'})
+             }else if(goodLIn == 3 ){
+                goodL.eq(1).css({marginLeft : '220px'})
+             }else if(goodLIn > 4){
+                goodU.css({height:'900px'})
+                if(goodLIn == 5){
+                  goodL.eq(3).css({marginLeft : '240px'});
+                }
+             }
+            }//for
+          })(this.jQuery);
