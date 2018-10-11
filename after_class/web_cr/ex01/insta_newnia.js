42eb27fdab7ec45a91d80e294ee484c22259a938
leedanbi <goldq0531@gmail.com>
2018-10-12 01:11:44 +0900

    remove folder node_modules

--- after_class/web_cr/ex01/insta_newnia.js
+++
@@ -1,6 +0,0 @@
-var client = require('cheerio-httpcli');
-
-client.fetch("https://www.instagram.com/newnia_official/", {}, function(err,$,res,body){
-	var bodym = $('body');
-	console.log(bodym.html());
-});
--- after_class/web_cr/ex01/naver.js
+++
@@ -1,7 +0,0 @@
-var client = require('cheerio-httpcli');
-client.fetch("http://comic.naver.com/webtoon/weekday.nhn", {}, function (err, $, res, body) {
-  var list = $(".list_area .col_selected li");
-  list.each(function(){
-    console.log($(this).find(".title").text());
-  });
-});
